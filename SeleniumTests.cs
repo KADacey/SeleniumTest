a14b21b87de2b3416360ffcbcf152d624d79934f
KADacey <kadacey@hotmail.com>
2015-06-08 18:34:45 -0700

    Small update

--- SeleniumTests.cs
+++ SeleniumTests.cs
@@ -37,6 +37,7 @@ namespace SeleniumTest
         /// This is done via a user who is not logged into the site.
         /// </summary>
         [TestMethod]
+        [DeploymentItem("PurchasePhoneTest.xml")]
         [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML", ".\\PurchasePhoneTest.xml", "Row", DataAccessMethod.Sequential)]
         public void PurchasePhoneTest_UserNotLoggedIn()
         {
@@ -101,6 +102,7 @@ namespace SeleniumTest
         /// and ensure the updated address has been saved between the logins.
         /// </summary>
         [TestMethod]
+        [DeploymentItem("UpdateAccountTest.xml")]
         [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML", ".\\UpdateAccountTest.xml", "Row", DataAccessMethod.Sequential)]
         public void UpdatedAccountAddressIsSaved()
         {
@@ -108,12 +110,6 @@ namespace SeleniumTest
             string username = Convert.ToString(context.DataRow["username"]);
             string password = Convert.ToString(context.DataRow["password"]);
             string expectedAddress = Convert.ToString(context.DataRow["expectedAddress"]);
-
-            //note: these values should come from an external source to make the test case data-driven
-            //      I'm hardcoding the values here just for expediency
-            //string expectedAddress = "789 Main Street";
-            //string givenUserName = "KADacey";
-            //string givenPassword = "APassword";
 
             //navigate to the given URL and maximize the browser
             driver.Navigate().GoToUrl(TestURL);
@@ -123,7 +119,7 @@ namespace SeleniumTest
             HomePage Home = new HomePage(driver);
 
             //logout if a user is already logged in
-            //Home.Logout();
+            Home.Logout();
 
             //navigate the the account page
             AccountPage Account = Home.NavigateToAccountPage();
@@ -191,12 +187,15 @@ namespace SeleniumTest
         /// validating that the empty cart message is shown as expected.
         /// </summary>
         [TestMethod]
+        [DeploymentItem("EmptyCartTest.xml")]
         [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML", ".\\EmptyCartTest.xml", "Row", DataAccessMethod.Sequential)]
         public void ValidateEmptyCartMessageAfterItemRemoved()
         {
+            Console.WriteLine(context.TestRunDirectory);
             //retrieve the test data
             string productDisplayName = Convert.ToString(context.DataRow["productDisplayName"]);
             string productTextName = Convert.ToString(context.DataRow["productTextName"]);
+            string expectedCartMsg = Convert.ToString(context.DataRow["emptyCartMsg"]);
 
             //navigate to the given URL and maximize the browser
             driver.Navigate().GoToUrl(TestURL);
@@ -220,7 +219,7 @@ namespace SeleniumTest
             //remove the item from the cart
             Checkout.RemoveItem();
 
-            Assert.IsTrue(Checkout.IsEmptyCartMessageShown("Oops, there is nothing in your cart."));
+            Assert.IsTrue(Checkout.IsEmptyCartMessageShown(expectedCartMsg));
         }
 
         [TestCleanup]
